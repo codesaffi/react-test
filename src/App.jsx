4a2136f2911bdcdf46201eceba46cb7b1b218324
codesaffi <saffi8946@gmail.com>
2024-06-27 16:52:19 +0500

    feat: routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
+     <nav>
+        <Link to="/vite-react-router/">Home</Link>
+        {" | "}
+        <Link to="/vite-react-router/contact">Contact</Link>
+      </nav>
+
+      <Outlet />
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from './pages/About.jsx';
+import Login from './pages/Login.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/react-test/",
+    element: <App />,
+    children: [
+      {
+        path: "/react-test/",
+        element: <About />,
+      },
+      {
+        path: "/react-test/contact",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
